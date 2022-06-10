d76b24530bb656c6bfd57a0c8eecd66b2bfbdf2b
arturo.menendez <arturo.menendez@MacBook-Pro-de-arturomenendez.local>
2022-06-10 18:48:46 +0200

    RED: createTodo calls create model method as a proper controller with request, response, next params

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,5 +1,7 @@
 const todoController = require("../../controllers/todo.controller");
 const todoModel = require("../../model/todo.model");
+const httpMocks = require("node-mocks-http");
+const newTodo = require("../mock-data/new-todo.json");
 
 //We expect mongoose to work, we focus in test our code. That's why the mock
 todoModel.create = jest.fn();
@@ -10,7 +12,12 @@ describe("todoController - createTodo",() => {
     });
 
     it("should call todoModel.create", () => {
-        todoController.createTodo();
-        expect(todoModel.create).toBeCalled();
-    })
+        let request, response, next;
+        request = httpMocks.createRequest();
+        response = httpMocks.createResponse();
+        next = null;
+        request.body = newTodo;
+        todoController.createTodo(request, response, next);
+        expect(todoModel.create).toBeCalledWith(newTodo);
+    });
 })
