1241310ecbbcdf13b1105241c3d3ce85b5ca2a04
arturo.menendez <arturo.menendez@MacBook-Pro-de-arturomenendez.local>
2022-06-13 15:50:16 +0200

    REFACTOR: refactored test structure to make it cleaner

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -6,16 +6,20 @@ const newTodo = require("../mock-data/new-todo.json");
 //We expect mongoose to work, we focus in test our code. That's why the mock
 todoModel.create = jest.fn();
 
+let request, response, next;
+
+beforeEach(() => {
+    request = httpMocks.createRequest();
+    response = httpMocks.createResponse();
+    next = null;
+});
+
 describe("todoController - createTodo",() => {
     it("should have a createTodo function", () => {
         expect(typeof todoController.createTodo).toBe("function");
     });
 
     it("should call todoModel.create", () => {
-        let request, response, next;
-        request = httpMocks.createRequest();
-        response = httpMocks.createResponse();
-        next = null;
         request.body = newTodo;
         todoController.createTodo(request, response, next);
         expect(todoModel.create).toBeCalledWith(newTodo);
