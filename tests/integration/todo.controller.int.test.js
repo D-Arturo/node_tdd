32f1b957fa39eee969f02f97646fa7566f9d4735
arturo.menendez <arturo.menendez@MacBook-Pro-de-arturomenendez.local>
2022-06-29 18:43:18 +0200

    Added more green integration test scenarios

--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -6,9 +6,7 @@ const endpointUrl = "/todos/";
 
 describe(`Given "/todos/" path`, () => {
   it("when POST endpoint is called, then should create a new Todo ", async () => {
-    const response = await request(app)
-      .post(endpointUrl)
-      .send(newTodo);
+    const response = await request(app).post(endpointUrl).send(newTodo);
 
     expect(response.status).toBe(201);
     expect(response.body.title).toBe(newTodo.title);
@@ -22,7 +20,26 @@ describe(`Given "/todos/" path`, () => {
 
     expect(response.status).toBe(500);
     expect(response.body).toStrictEqual({
-      message: "Todo validation failed: done: Path `done` is required."
-    })
+      message: "Todo validation failed: done: Path `done` is required.",
+    });
+  });
+
+  it("when POST endpoint is called with missing 'title' field, then a proper error should be displayed ", async () => {
+    const response = await request(app).post(endpointUrl).send({ done: true });
+
+    expect(response.status).toBe(500);
+    expect(response.body).toStrictEqual({
+      message: "Todo validation failed: title: Path `title` is required.",
+    });
+  });
+
+  it("when POST endpoint is called with wrong 'done' type, then a proper error should be displayed ", async () => {
+    const response = await request(app)
+      .post(endpointUrl)
+      .send({ title: "wrong 'done' type", done: "123" });
+
+    expect(response.status).toBe(500);
+    console.log(response.body);
+    expect(response.body.message).toMatch(/Todo validation failed: done: Cast to Boolean failed/);
   });
 });
